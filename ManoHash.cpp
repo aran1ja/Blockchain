576dab37bed8dedc5edcd2d8c393044c2b9ee091
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-09-23 17:46:43 +0300

    Add files via upload

--- ManoHash.cpp
+++ ManoHash.cpp
@@ -13,12 +13,23 @@ cout << "2. Atidaryti failus, sudarytus tik is vieno simbolio." << endl;
 cout << "3. Atidaryti programos sugeneruotus failus, kuriuose yra > 1000 simboliu." << endl;
 cout << "4. Atidaryti faila tekstas_1000.txt, kuriame yra > 1000 simboliu." << endl;
 cout << "5. Atidaryti faila tekstas_1000_kitas.txt, kuris skiriasi nuo tekstas_1000.txt tik vienu simboliu." << endl;
-cout << "6. Baigti darba." << endl;
+cout << "6. Atidaryti tuscia faila 'tuscias.txt'." << endl;
+cout << "7. Baigti darba." << endl;
 cout << "Jusu pasirinkimas: "; cin >> pasirinkimas;
 
 switch (pasirinkimas) {
     case 1:
     {
+        string simboliu_seka;
+        cout << "Iveskite simboliu seka, kuria noretumete uzkoduoti: "; cin >> simboliu_seka;
+        int ilgis = 8 * simboliu_seka.size();
+        cout << "Jusu zodis susideda is " << ilgis << " bitu." << endl;
+
+        // Zodis isverciamas i ASCII
+        for (char simbolis : simboliu_seka) {
+            cout << "Simbolis " << simbolis << " uzkoduotas kaip " << (int)simbolis << " pagal ASCII" << endl;
+        }
+
 
     }
     case 2:
@@ -38,6 +49,10 @@ switch (pasirinkimas) {
         
     }
     case 6:
+    {
+        
+    }
+    case 7:
     {
         cout << "Darbas baigtas." << endl;
         break;
